7cb01f59ded3deabbf1dcbea30c396703987611f
ltgithub2008 <44565080+ltgithub2008@users.noreply.github.com>
2018-10-29 22:26:15 +0800

    Update viewlogs.py

--- app/viewlogs.py
+++ app/viewlogs.py
@@ -7,8 +7,6 @@ import funct
 import sql
 from jinja2 import Environment, FileSystemLoader
 
-import codecs, sys
-sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
 
 env = Environment(loader=FileSystemLoader('templates/'))
 template = env.get_template('logs.html')
